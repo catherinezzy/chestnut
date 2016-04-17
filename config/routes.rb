76b427abf79bbec6635e1981c5a07ac1bc3d1d6c
Ziyue <catherine000000@gmail.com>
2016-04-17 21:55:08 +0000

    setting up database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Chestnut::Application.routes.draw do
+  root 'events#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
