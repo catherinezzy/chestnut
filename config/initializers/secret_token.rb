59492d3a7ffaaf7ec15f315b9135954ed04486d2
Ziyue <catherine000000@gmail.com>
2016-04-17 21:46:58 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chestnut::Application.config.secret_key_base = 'e54c2d838cfd903f533249a30fb7c49696c373a9c05657a38d898ce77f1cf13c1d189098150a2d5ea2712adb273300d3af06c86994c93e12858728d0144eb72b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chestnut::Application.config.session_store :cookie_store, key: '_chestnut_session'
